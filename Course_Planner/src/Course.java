b8ec4b800e9528a58b7d582441a4b226bc1f0426
Barricature <irisxia2016@gmail.com>
2022-02-05 12:44:14 -0500

    Created Course Class

---
+++ Course_Planner/src/Course.java
@@ -0,0 +1,45 @@
+public class Course {
+    private String courseNum;
+    private String courseName;
+    private int credits;
+
+    public Course (int credits) {
+        this.credits = credits;
+    }
+
+    public Course (String courseNum, int credits) {
+        this.courseNum = courseNum;
+        this.credits = credits;
+    }
+    }
+
+    public Course (String courseNum, String courseName, int credits) {
+        this.courseNum = courseNum;
+        this.courseName =  courseName;
+        this.credits = credits;
+    }
+
+    public String getNum () {
+        return this.courseName;
+    }
+
+    public void setNum (String courseNum) {
+        this.courseNum = courseNum;
+    }
+
+    public String getName () {
+        return this.courseName;
+    }
+
+    public void setName (String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getCredits () {
+        return this.credits;
+    }
+
+    public void setCredits (int credits) {
+        this.credits = credits;
+    }
+}
