2bc963ca9e1fc333b63110b073903ca7a1352499
Barricature <irisxia2016@gmail.com>
2022-08-29 02:28:41 -0400

    grid display

--- resources/js/main.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("click", function(e) {
-    let span = document.createElement("span");
-    span.innerHTML = "something";
-    span.style.position = "absolute";
-    span.style.transition = "all 0.5s"
-}, 1000)
